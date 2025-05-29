3de32a2f433d14822685bbb5557a5e132de7dceb
joypauls <joypaulsen3@gmail.com>
2025-05-28 20:39:12 -0500

    reset og image for test

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  arrowParens: 'always',
+  arrowParens: "always",
   singleQuote: false,
   jsxSingleQuote: false,
   tabWidth: 2,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.png`],
+    images: [`${siteConfig.url}/images/og.jpg`],
     type: "website",
     locale: "en_US",
   },
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: siteConfig.title,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/images/og.png`],
+    images: [`${siteConfig.url}/images/og.jpg`],
     // creator: '@th_clarence',
   },
   // authors: [
